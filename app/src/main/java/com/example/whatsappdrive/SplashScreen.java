ca06bbec6c98097b93d524da03fce8552d0a6ee4
ishita1521 <76198031+ishita1521@users.noreply.github.com>
2022-07-30 17:02:43 +0530

    WhatsApp Drive is an app that can store all the data from your WhatsApp so that you can watch it later. It shows all the statuses and you can save them and watch them later. It can also save your own status. WhatsApp Drive stores all your Images, videos, Audios, Documents, GIfs,Stickers from your WhatsApp to make them readily available for you.
    WhatsApp Drives is built on MVVM architecture and uses Java as the Coding Language.

--- app/src/main/java/com/example/whatsappdrive/SplashScreen.java
+++ app/src/main/java/com/example/whatsappdrive/SplashScreen.java
@@ -1,16 +1,34 @@
 package com.example.whatsappdrive;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.app.AppOpsManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.view.WindowManager;
+
+import java.util.Objects;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashScreen extends AppCompatActivity {
+
     final static int splash_delay=2000;
+    private static final int REQUEST_PERMISSIONS = 1234;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private static final String MANAGE_EXTERNAL_STORAGE_PERMISSION = "android:manage_external_storage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +36,75 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        if (!arePermissionDenied()){
+            next();
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionDenied()) {
+
+            // If Android 11 Request for Manage File Access Permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+
+                startActivityForResult(intent, REQUEST_PERMISSIONS);
+                return;
+            }
+
+            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+        }
+    }
+
+    private void next() {
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashScreen.this,MainActivity.class));
             finish();
         },splash_delay);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!arePermissionDenied()) {
+            next();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    boolean checkStorageApi30() {
+        AppOpsManager appOps = getApplicationContext().getSystemService(AppOpsManager.class);
+        int mode = appOps.unsafeCheckOpNoThrow(
+                MANAGE_EXTERNAL_STORAGE_PERMISSION,
+                getApplicationContext().getApplicationInfo().uid,
+                getApplicationContext().getPackageName()
+        );
+        return mode != AppOpsManager.MODE_ALLOWED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS && grantResults.length > 0) {
+            if (arePermissionDenied()) {
+                // Clear Data of Application, So that it can request for permissions again
+                ((ActivityManager) Objects.requireNonNull(this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+                recreate();
+            } else {
+                next();
+            }
+        }
+    }
+
+    private boolean arePermissionDenied() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return checkStorageApi30();
+        }
+
+        for (String permissions : PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), permissions) != PackageManager.PERMISSION_GRANTED) {
+                return true;
+            }
+        }
+        return false;
     }
 }
